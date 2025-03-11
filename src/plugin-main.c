1ecc886c539d43f83ab6f40939c7f5d0d7c5bd44
Jakob <minenash@protonmail.com>
2025-03-11 01:05:35 -0400

    Initial commit

---
+++ src/plugin-main.c
@@ -0,0 +1,17 @@
+#include <obs-module.h>
+#include <plugin-support.h>
+#include <QWidget>
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+bool obs_module_load(void)
+{
+	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	return true;
+}
+
+void obs_module_unload(void)
+{
+	obs_log(LOG_INFO, "plugin unloaded");
+}
